8a8289e43256b4edb68677bf25414c29b4b4735e
Ariq Mukul <ariqmukul@gmail.com>
2024-05-12 00:17:07 -0400

    trying to fix deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,21 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ariq | Portfolio",
+  title: "Ariq Mukul",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* Or specify the file type if using a different format */}
+        {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
